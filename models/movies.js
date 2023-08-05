2a4ad7d034a57c648305e4bd04de5d4abd25ae6f
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-08-05 22:10:28 +0330

    Embed modeling

--- models/movies.js
+++ models/movies.js
@@ -1,28 +1,23 @@
 const mongoose = require('mongoose');
 
+const genreSchema = new mongoose.Schema({
+  name: { type: String, required: true, lowercase: true }
+});
+
 const movieSchema = new mongoose.Schema({
-  title: {
+  name: {
     type: String,
     required: true,
     trim: true,
     minlength: 3,
     maxlength: 55
   },
-  genre: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Genre',
-    required: true
-  },
   price: {
     type: Number,
     required: true,
     min: 0
   },
-  dailyRentalRate: {
-    type: Number,
-    required: true,
-    min: 0
-  }
+  genre: genreSchema
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
--- routes/movies.js
+++ routes/movies.js
@@ -1,42 +1,126 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const Movie = require('../models/movies');
-const { Genre } = require('../models/genre');
+const Movie = require('../models/movies'); // Import the Movie model
 
 router.use(express.json());
 
-async function createMovie(title, price, dailyRentalRate, genreName) {
+// Validation middleware for POST route
+const postValidationData = [
+    body('name').notEmpty().withMessage('Name is required').isLength({ min: 3 }).withMessage('Name must be at least 3 characters'),
+    body('price').notEmpty().withMessage('Price is required').isNumeric().withMessage('Price must be a number'),
+    body('dailyRate').notEmpty().withMessage('Daily rate is required').isNumeric().withMessage('Daily rate must be a number'),
+    body('genre.name').notEmpty().withMessage('Genre name is required').isLength({ min: 2 }).withMessage('Genre name must be at least 2 characters'),
+];
+
+
+async function createMovie() {
+    const movie = new Movie({
+        name: "Test Movie",
+        price: 10,
+        dailyRate: 4,
+        genre: {
+            name: "drama",
+        },
+    });
     try {
-        const genre = await Genre.findOne({ name: genreName });
-        if (!genre) {
-            console.log('Genre not found.');
-            return;
-        }
-
-        const movie = new Movie({
-            title,
-            price,
-            dailyRentalRate,
-            genre: genre._id
-        });
-
         const result = await movie.save();
-        console.log('New movie created:', result);
+        console.log("Created movie:", result);
     } catch (ex) {
-        console.error('Error creating movie:', ex);
+        console.error("Error creating movie:", ex);
     }
 }
 
-router.get('/test', async (req, res) => {
+//createMovie();
+
+router.get('/', async (req, res) => {
+    const movies = await Movie.find();
+    res.send(movies);
+});
+
+router.get('/:id', async (req, res) => {
+    const givenID = req.params.id;
     try {
-        createMovie('Interstellar', 15, 4, 'drama');
-        res.send('Movie created successfully.');
-    } catch (error) {
-        res.status(500).send('Something went wrong');
+        const founded_movie = await Movie.findById(givenID);
+        if (!founded_movie) {
+            return res.status(404).send("The movie with the given id was not found");
+        } else {
+            res.send(founded_movie);
+        }
+    } catch (exp) {
+        console.error(exp);
     }
 });
 
-// Other routes for getting, updating, and deleting movies
+router.post('/', postValidationData, async (req, res) => {
+    console.log(req.body);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array().map((err) => err.msg));
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const movie = new Movie({
+        name: req.body.name,
+        price: req.body.price,
+        dailyRate: req.body.dailyRate,
+        genre: {
+            name: req.body.genre.name,
+        },
+    });
+    try {
+        const result = await movie.save();
+        res.send(movie);
+        console.log(result);
+    } catch (ex) {
+        console.error("Error creating movie:", ex);
+    }
+});
+
+const putValidationData = [
+    body('name').notEmpty().withMessage('Name is required').isLength({ min: 3 }).withMessage('Name must be at least 3 characters'),
+    body('price').notEmpty().withMessage('Price is required').isNumeric().withMessage('Price must be a number'),
+    body('dailyRate').notEmpty().withMessage('Daily rate is required').isNumeric().withMessage('Daily rate must be a number'),
+    body('genre.name').notEmpty().withMessage('Genre name is required').isLength({ min: 2 }).withMessage('Genre name must be at least 2 characters'),
+];
+
+router.put('/:id', putValidationData, async (req, res) => {
+    const givenID = req.params.id;
+    try {
+        const founded_movie = await Movie.findByIdAndUpdate(
+            givenID,
+            {
+                name: req.body.name,
+                price: req.body.price,
+                dailyRate: req.body.dailyRate,
+                genre: {
+                    name: req.body.genre.name,
+                },
+            },
+            { new: true } // Return the updated movie
+        );
+        if (!founded_movie) {
+            return res.status(404).send(`The movie with given id: ${givenID} was not found`);
+        } else {
+            res.send(founded_movie);
+        }
+    } catch (exp) {
+        res.status(500).send('Something went wrong');
+        console.error(exp);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const movieID = req.params.id;
+    try {
+        const result = await Movie.findByIdAndDelete(movieID);
+        console.log(result);
+        return res.status(200).send("Deleted");
+    } catch (exp) {
+        res.status(500).send('Something went wrong');
+        console.error(exp);
+    }
+});
 
 module.exports = router;
