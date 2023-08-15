655d6e8c799f18586ffdb80749bed5605d5a1bc9
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-08-15 20:25:54 +0330

    Unit test with Jest

---
+++ tests/unit/models/users.test.js
@@ -0,0 +1,19 @@
+const User = require('../../../models/users');
+const users = require('../../../models/users');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const config = require('config');
+const SecretKey = config.get('jwtPrivateKey');
+
+describe('user.generateAuthToken', () => {
+    it('should return a valid token', () => {
+        const payload = ({ _id: new mongoose.Types.ObjectId().toHexString(), isAdmin: true });
+        const user = new User(payload);
+
+
+        const token = jwt.sign(payload, SecretKey);
+        const decodeToken = jwt.verify(token, SecretKey);
+        expect(decodeToken.isAdmin).toBe(user.isAdmin);
+        expect(decodeToken._id).toBe(payload._id);
+    });
+});
