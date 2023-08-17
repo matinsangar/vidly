57d2210812c5fbad8fd8f7385aa21597f3d8e353
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-08-17 13:38:42 +0330

    First Integration test

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ const auth_middleware = require('./middlewares/auth');
 app.use(auth_middleware);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+module.exports = server;
