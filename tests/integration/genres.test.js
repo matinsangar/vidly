282a0e8a8e905734f0eec9e3ff69986077f05f78
Matin Amin Sabouti <matinaminsabouri70@gmail.com>
2023-08-19 13:25:16 +0330

    Testing routes with params

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -25,6 +25,19 @@ describe('/api/genres', () => {
             expect(res.body.length).toBe(2);
             expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
             expect(res.body.some(g => g.name === 'genre2')).toBeTruthy(); //some method : It checks if at least one element in the array satisfies a certain condition
-        }, 10000);  //Fix timeout issue we can add timer 
+        });  //Fix timeout issue we can add timer 
+    });
+    describe('GET /:id', () => {
+        it('should return a valid Genre with valid given ID', async () => {
+            const genre = new Genre({ name: "genre1" });
+            await genre.save();
+            const res = await request(server).get("/api/genres/" + genre._id);
+            expect(res.status).toBe(200);
+            expect(res.body).toHaveProperty("name", genre.name);
+        });
+        it('should return 404 status if invalid id is passed', async () => {
+            const res = await request(server).get("/api/genres/887987987987");
+            expect(res.status).toBe(404);
+        });
     });
 });
